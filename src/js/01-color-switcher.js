50dfd1bc21749debfaa838c50605bd0c24d1e143
Ivan Borovyk <Ivan_Borovyk@epam.com>
2023-11-12 10:56:06 +0200

    task 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const body = document.querySelector('body');
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
 
+function onStartButtonClick() {
+    startButton.disabled = true;
+    intervalId = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+}
+
+function onStopButtonClick() {
+    startButton.disabled = false;
+    clearInterval(intervalId);
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+let intervalId = null;
+
+startButton.addEventListener('click', onStartButtonClick);
+stopButton.addEventListener('click', onStopButtonClick);
