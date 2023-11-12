b7bbbf13cd0ef44fe81caf39264e512e8a8f292f
Ivan Borovyk <Ivan_Borovyk@epam.com>
2023-11-12 12:54:31 +0200

    task 3 done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,38 @@
+import iziToast from "izitoast";
+
+import "izitoast/dist/css/iziToast.min.css";
+
+const form = document.querySelector('.form');
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      shouldResolve ? resolve({ position, delay }) : reject({ position, delay });
+    }, delay);
+  });
+}
+
+function onSubmit(event) {
+  event.preventDefault();
+  const formData = new FormData(event.currentTarget);
+  const amount = Number(formData.get('amount'));
+  const step = Number(formData.get('step'));
+  const delay = Number(formData.get('delay'));
+
+  for (let i = 0; i < amount; i += 1) {
+    createPromise(i, delay + step * i).then(({ position, delay }) => {
+      iziToast.success({
+        title: 'Success',
+        message: `Promise ${position} resolved after ${delay}ms`,
+      });
+    }).catch(({ position, delay }) => {
+      iziToast.error({
+        title: 'Error',
+        message: `Promise ${position} rejected after ${delay}ms`,
+      });
+    });
   }
 }
+
+form.addEventListener('submit', onSubmit);
