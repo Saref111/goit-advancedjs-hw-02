ba1cf9bed318bcd0a2279ee11190d77fe5942e0c
Ivan Borovyk <Ivan_Borovyk@epam.com>
2023-11-13 11:57:55 +0200

    fix

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,6 +1,7 @@
 const body = document.querySelector('body');
 const startButton = document.querySelector('button[data-start]');
 const stopButton = document.querySelector('button[data-stop]');
+let intervalId = null;
 
 function onStartButtonClick() {
     startButton.disabled = true;
@@ -17,8 +18,6 @@ function onStopButtonClick() {
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
-
-let intervalId = null;
 
 startButton.addEventListener('click', onStartButtonClick);
 stopButton.addEventListener('click', onStopButtonClick);
